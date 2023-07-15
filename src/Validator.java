4a1d1602c144b48946d8a98857e6a477d108628a
kimlevcenko <levchenko.kim@icloud.com>
2023-07-15 22:00:26 +0500

    Fix2 ДЗ Exception

--- src/Validator.java
+++ src/Validator.java
@@ -5,10 +5,10 @@ public class Validator {
     private static final Pattern PATTERN = Pattern.compile("[a-zA-Z0-9_]{0,20}+$");
 
     public static void check(String login, String password, String confirmPassword) throws WrongLoginException {
-        if (login.length() > 20 || !PATTERN.matcher(login).matches()) {
+        if (!PATTERN.matcher(login).matches()) {
             throw new WrongLoginException("Логин содержит недопустимые символы или длинее 20 знаков!");
         }
-        if (password.length() > 20 || !PATTERN.matcher(password).matches()) {
+        if (!PATTERN.matcher(password).matches()) {
             throw new WrongPasswordException("Пароль содержит недопустимые символы или длинее 20 символов!");
         }
         if (!password.equals(confirmPassword)) {
